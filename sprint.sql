fac8a56a4524d8b06ceeb74fe369ab7161397029
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2024-11-03 12:57:47 -0330

    customer table

--- sprint.sql
+++ sprint.sql
@@ -6,3 +6,11 @@ CREATE TABLE IF NOT EXISTS Movies (
   genre VARCHAR(50) NOT NULL,
   director VARCHAR(255) NOT NULL
 );
+-- Customer Table
+CREATE TABLE IF NOT EXISTS Customers (
+  customer_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  phone VARCHAR(15)
+);
